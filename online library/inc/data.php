bedaceb25abc1a9a662ff6f528620004678f075e
HALEY W <50425738+haleywhitehead@users.noreply.github.com>
2019-06-03 18:10:58 -0400

    Add files via upload

---
+++ online library/inc/data.php
@@ -0,0 +1,178 @@
+<?php
+
+///Books
+$catalog[101] = [
+  "title" => "1984",
+  "img" => "img/1984.jpg",
+ "genre" => "Science-fiction",
+ "format" => "Paperback",
+ "year" => 1949,
+ "category" => "Books",
+ "authors" => [
+     "George Orwell"
+ ],
+ "publisher" => "Harvill Secker",
+ "isbn" => '9781497490826'
+];
+
+$catalog[102] = [
+  "title" => "The Great Gatsby",
+  "img" => "img/great.jpg",
+ "genre" => "Fiction",
+ "format" => "Paperback",
+ "year" => 1925,
+ "category" => "Books",
+ "authors" => [
+     "F. Scott Fitzgerald"
+ ],
+ "publisher" => "Charles Scribner's Sons",
+ "isbn" => '9781505612844'
+];
+
+$catalog[103] = [
+  "title" => "The Lord of the Flies",
+  "img" => "img/flies.jpg",
+ "genre" => "Allegory",
+ "format" => "Paperback",
+ "year" => 1954,
+ "category" => "Books",
+ "authors" => [
+     "William Golding"
+ ],
+ "publisher" => "Faber and Faber",
+ "isbn" => '9780399501487'
+];
+
+$catalog[104] = [
+  "title" => "The Hunger Games",
+  "img" => "img/hunger.jpg",
+ "genre" => "Science-Fiction",
+ "format" => "Paperback",
+ "year" => 2008,
+ "category" => "Books",
+ "authors" => [
+     "Suzanne Collins"
+ ],
+ "publisher" => "Scholastic Corporation",
+ "isbn" => '9788852023729'
+];
+
+///Movies
+$catalog[201] = [
+ "title" => "Archie's Final Project",
+ "img" => "img/archie.jpg",
+ "genre" => "Comedy,Drama",
+ "format" => "DVD",
+ "year" => 2009,
+ "category" => "Movies",
+ "director" => "David Lee Miller",
+ "writers" => [
+     "Eric J. Adams",
+     "David Lee Miller"
+
+ ],
+ "stars" => [
+     "David Carradine",
+     "Mariel Hemingway",
+     "Brooke Nevin"
+ ]
+];
+
+$catalog[202] = [
+ "title" => "Incredibles 2",
+ "img" => "img/incredibles.jpg",
+ "genre" => "Comedy, Animation",
+ "format" => "DVD",
+ "year" => 2018,
+ "category" => "Movies",
+ "director" => "Brad Bird",
+ "writers" => [
+     "Brad Bird"
+
+ ],
+ "stars" => [
+     "Craig T. Nelson",
+     "Holly Hunter",
+     "Sarah Vowell"
+ ]
+];
+
+$catalog[203] = [
+ "title" => "Brick",
+ "img" => "img/brick.jpeg",
+ "genre" => "Action,Crime",
+ "format" => "DVD",
+ "year" => 2005,
+ "category" => "Movies",
+ "director" => "Rian Johnson",
+ "writers" => [
+     "Rian Johnson"
+
+ ],
+ "stars" => [
+     "Joseph Gordon-Levitt",
+     "Lukas Haas",
+     "Emilie de Ravin"
+ ]
+];
+
+$catalog[204] = [
+ "title" => "Nick &amp; Norah's Infinite Playlist",
+ "img" => "img/nn.jpg",
+ "genre" => "Comedy,Drama",
+ "format" => "DVD",
+ "year" => 2008,
+ "category" => "Movies",
+ "director" => "Peter Sollett",
+ "writers" => [
+     "Lorene Scafaria",
+     "Rachel Cohn"
+
+ ],
+ "stars" => [
+     "Michael Cera",
+     "Kat Dennings",
+     "Aaron Yoo"
+ ]
+];
+
+///Music
+$catalog[301] = [
+ "title" => "Lean Into It",
+ "img" => "img/allison.jpg",
+ "genre" => "Pop,Punk,Lo-Fi",
+ "format" => "CD",
+ "year" => "2014",
+ "category" => "Music",
+ "artist" => "Allison Crutchfield"
+];
+
+$catalog[302] = [
+ "title" => "Body War",
+ "img" => "img/smtb.jpg",
+ "genre" => "Punk",
+ "format" => "CD",
+ "year" => "2016",
+ "category" => "Music",
+ "artist" => "Show Me the Body"
+];
+
+$catalog[303] = [
+ "title" => "Pottymouth",
+ "img" => "img/brat.jpg",
+ "genre" => "Punk Rock,Riot grrrl,Indie-rock",
+ "format" => "CD",
+ "year" => "1993",
+ "category" => "Music",
+ "artist" => "Bratmobile"
+];
+
+$catalog[304] = [
+ "title" => "Going Grey",
+ "img" => "img/tfb.jpeg",
+ "genre" => "Alternative,Indie",
+ "format" => "CD",
+ "year" => "2017",
+ "category" => "Music",
+ "artist" => "The Front Bottoms"
+];
